d7f5239eb12fb53f69575b094ae4d9bca7e04092
OMR K9 <121472588+oreynosa12@users.noreply.github.com>
2023-04-09 22:12:37 -0400

    Added server.js file

---
+++ Develop/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware for serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+// TODO: Implement logic to read notes from file and send them back as response
+app.get('/api/notes', (req, res) => { 
+    const tipId = req.params.tip_id;
+    readFromFile('./api/notes')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((tip) => tip.tip_id === tipId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No tip with that ID');
+      });
+  
+});
+
+app.post('/api/notes', (req, res) => {
+  // TODO: Implement logic to save a new note to file
+
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  // TODO: Implement logic to delete a note from file based on ID
+  
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server started listening on port ${PORT}`);
+});
