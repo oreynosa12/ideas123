1509bc2f3d6ce9808a70495ac69d12656432f926
OMR K9 <121472588+oreynosa12@users.noreply.github.com>
2023-04-11 01:01:38 -0400

    added readfile and writefile code to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
+const readFromFile = util.promisify(fs.readFile);
+const writeToFile = util.promisify(fs.writeFile);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,7 +23,7 @@ app.get('/notes', (req, res) => {
 // TODO: Implement logic to read notes from file and send them back as response
 app.get('/api/notes', (req, res) => { 
     const tipId = req.params.tip_id;
-    readFromFile('./api/notes')
+    readFromFile('./db/db.json')
       .then((data) => JSON.parse(data))
       .then((json) => {
         const result = json.filter((tip) => tip.tip_id === tipId);
@@ -31,10 +34,14 @@ app.get('/api/notes', (req, res) => {
   
 });
 
+
+
 app.post('/api/notes', (req, res) => {
   // TODO: Implement logic to save a new note to file
-
+  const newNote = req.body;
 });
+
+writeToFile('./db/db.json', newNote)
 
 app.delete('/api/notes/:id', (req, res) => {
   // TODO: Implement logic to delete a note from file based on ID
